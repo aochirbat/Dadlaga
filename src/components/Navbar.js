ce6b5c096401aa54481076417d7571f636a6f3f5
Erbold <walsmgl@gmail.com>
2020-05-25 22:54:49 +0800

    Ebogos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,246 +1,82 @@
 import React from 'react';
-import { fade, makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import VpnKeyTwoToneIcon from '@material-ui/icons/VpnKeyTwoTone';
-import HomeTwoToneIcon from '@material-ui/icons/HomeTwoTone';
-import MoreIcon from '@material-ui/icons/MoreVert';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link as Linkdom,
-  useRouteMatch,
-  useParams
-} from 'react-router-dom';
+import IconButton from '@material-ui/core/IconButton'
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+import Button from '@material-ui/core/Button';
+
+import { Container, Grid } from '@material-ui/core';
+import 
+{BrowserRouter as Router,Switch,Route,Link as Linkdom}
+from 'react-router-dom';
+
+import z from '../Images/5.png'
+
 const useStyles = makeStyles((theme) => ({
-  grow: {
-    flexGrow: 1,
-  },
-  head: {
-    backgroundColor: "#fff",
-    position: "sticky",
-    top: 0
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
   title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-  sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-    },
-  },
-  sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
+    flexGrow: 1,
+   
+   
+        fontSize: '1.5rem',
+      
+        fontFamily: 'Segoe UI Black'
   },
 }));
-export default function Main() {
-    const classes = useStyles();
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-  
-    const isMenuOpen = Boolean(anchorEl);
-    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-  
-    const handleProfileMenuOpen = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-    const handleMobileMenuClose = () => {
-      setMobileMoreAnchorEl(null);
-    };
-    const handleMenuClose = () => {
-      setAnchorEl(null);
-      handleMobileMenuClose();
-    };
-    const handleMobileMenuOpen = (event) => {
-      setMobileMoreAnchorEl(event.currentTarget);
-    };
-    const menuId = 'primary-search-account-menu';
-    const renderMenu = (
-      <Menu
-        anchorEl={anchorEl}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        id={menuId}
-        keepMounted
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        open={isMenuOpen}
-        onClose={handleMenuClose}
-      >
-        <MenuItem to="/Login"  component={Linkdom} ><VpnKeyTwoToneIcon/> Нэвтрэх</MenuItem>
-        <MenuItem onClick={handleMenuClose}>Гарах</MenuItem>
-      </Menu>
-    );
-    const mobileMenuId = 'primary-search-account-menu-mobile';
-    const renderMobileMenu = (
-      <Menu
-        anchorEl={mobileMoreAnchorEl}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        id={mobileMenuId}
-        keepMounted
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        open={isMobileMenuOpen}
-        onClose={handleMobileMenuClose}
-      >
-        <MenuItem>
-          <IconButton to="/notif" component={Linkdom} aria-label="show 4 new mails" color="inherit"> 
 
-            <Badge badgeContent={4} color="secondary">
-              <MailIcon/>
-            </Badge>
-          </IconButton>
-          <p>Messages</p>
-        </MenuItem>
-        <MenuItem>
-          <IconButton to="/Customer" component={Linkdom} aria-label="show 11 new notifications" color="inherit">
-            <Badge badgeContent={11} color="main">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-          <p>Notifications</p>
-        </MenuItem>
-        <MenuItem onClick={handleProfileMenuOpen}>
-          <IconButton
-            aria-label="account of current user"
-            aria-controls="primary-search-account-menu"
-            aria-haspopup="true"
-            color="inherit"
-          >
-            <AccountCircle />
-          </IconButton>
-          <p>Profile</p>
-        </MenuItem>
-      </Menu>
-    );
-  
-    return (
-      <div className={classes.grow}>
-        <AppBar position="fixed">
-          <Toolbar>
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="open drawer"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography className={classes.title} variant="h6" noWrap>
-              Спорт/Фитнесс 
-            </Typography>
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search…"
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-                inputProps={{ 'aria-label': 'search' }}
-              />
-            </div>
-            <div className={classes.grow} />
-            <div className={classes.sectionDesktop}>
-             <IconButton to="/" component={Linkdom} aria-label="show 4 new mails" color="inherit">
-                <Badge badgeContent={4} color="secondary">
-                  <HomeTwoToneIcon />
-                </Badge>
-              </IconButton>
-              <IconButton to="/notif" component={Linkdom} aria-label="show 4 new mails" color="inherit">
-                <Badge badgeContent={4} color="secondary">
-                  <MailIcon />
-                </Badge>
-              </IconButton>
-              <IconButton to="/Customer" component={Linkdom} aria-label="show 17 new notifications" color="inherit">
-                <Badge badgeContent={17} color="secondary">
-                  <NotificationsIcon />
-                </Badge>
-              </IconButton>
-              <IconButton
-                edge="end"
-                aria-label="account of current user"
-                aria-controls={menuId}
-                aria-haspopup="true"
-                onClick={handleProfileMenuOpen}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-            </div>
-            <div className={classes.sectionMobile}>
-              <IconButton
-                aria-label="show more"
-                aria-controls={mobileMenuId}
-                aria-haspopup="true"
-                onClick={handleMobileMenuOpen}
-                color="inherit"
-              >
-                <MoreIcon />
-              </IconButton>
-            </div>
-          </Toolbar>
-        </AppBar>
-        {renderMobileMenu}
-        {renderMenu}
-      </div>
-)
-            }
+
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <AppBar style={{ background: 'transparent', boxShadow: 'none'}}>
+        <Toolbar>
+          
+        <Grid container direction="row" xs={12}>
+                
+                   
+                    <Grid container xs={4} className={classes.paper}>
+                      <img src={z} width="150" heigth="100"/>
+                      </Grid>
+      
+                      <Grid container xs={4} direction="row" className={classes.paper} justify="flex-end"
+              alignItems="center">
+                            <Grid item xs={4}  >
+                              <Button  className={classes.title} color="secondary"  > 
+                              Home
+                          </Button>
+                            </Grid>
+                      
+                      
+
+                            <Grid item xs={4} >
+                            <Button className={classes.title}  color="Secondary">
+                            About
+                              </Button>
+                            </Grid>
+                      
+                      
+
+                            <Grid item xs={4} >
+                            <Button  className={classes.title} color="Secondary">
+                            Support 
+                            </Button>
+                            </Grid>
+                      </Grid>
+          
+                      
+                      <Grid container xs={4} justify="flex-end"
+              alignItems="center">
+                      <IconButton to='/login' component={Linkdom}>
+                        <AccountCircleOutlinedIcon fontSize='large'/>
+                      </IconButton>
+                      </Grid>
+
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
